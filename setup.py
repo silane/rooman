c2533af9baf103a41c6fba5cc2db8085b9e6011c
silane <18604696+silane@users.noreply.github.com>
2023-01-16 00:00:14 +0900

    Upgrade version to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rooman',
-    version='1.0.0',
+    version='1.0.1',
     description='Very simple room automation framework with web API',
     author='silane',
     author_email='silane@silane8.net',
